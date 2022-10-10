ef5a413ed386e613a16fc4d98e3330a87328840b
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-10-10 23:24:13 +0200

    task 1.4 done

---
+++ LAB_1/task_1_3.m
@@ -0,0 +1,23 @@
+% TASK 1.4
+
+h = [1,2,3,4],
+v = [5;6;7;8],
+
+a=h*v,
+
+A=v*h,
+
+a=A*v,
+
+b=h*A,
+
+B=A*A,
+
+% Z=h*h,
+% X=v*v,
+% C=v*A,
+% F=A*h,
+%
+% zle rozmiary macierzy
+% liczba kolumn w pierwszej macierzy musi byc rowna 
+% liczbie rzzedow w drugiej
