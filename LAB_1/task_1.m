a0c265d9e2a08fe05d5bdacfceb065a08e7e2b7a
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-10-10 16:20:42 +0200

    test

---
+++ LAB_1/task_1.m
@@ -0,0 +1,33 @@
+clear all; close all;
+echo on
+
+%1.1.1
+%radius
+r=10;
+
+%surf
+p=pi*(r^2),
+%circle
+o=2*pi*r,
+
+%1.1.2
+
+sv=4*pi*(r^2),
+v=4/3*pi*(r^3),
+
+%1.2.1
+
+a=1;
+b=5;
+c=6;
+
+%delta
+d=b^2-4*a*c,
+
+p1=(-b-sqrt(d))/2*a,
+p2=(-b+sqrt(d))/2*a,
+
+%check
+check=a*p1^2+b*p1+c,
+
+whos
---
+++ LAB_1/test.m
@@ -0,0 +1,9 @@
+clear all; close all;
+echo on
+
+a=1; b=3;
+c=a+b,
+d=sqrt(c),
+e =a+j*b,
+f=conj(e),
+whos
