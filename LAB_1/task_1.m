a899144d339f572dbfbd998e6f3235fa66f3ae10
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-10-10 23:55:57 +0200

    task 1.1 ; 1.2 done

--- LAB_1/task_1.m
+++ LAB_1/task_1.m
@@ -1,35 +1,39 @@
 clear all; close all;
 echo on
 
-%1.1.1
+% TASK 1.1.1
 %radius
 r=10;
 
 %surf
-p=pi*(r^2),
+S=pi*(r^2),
 %circle
-o=2*pi*r,
+O=2*pi*r,
 
-%1.1.2
+% TASK 1.1.2
 
 sv=4*pi*(r^2),
 v=4/3*pi*(r^3),
 
-%1.2.1
+% TASK 1.2
 
 a=1;
 b=5;
-c=6;
+c=600;
 
 %delta
 d=b^2-4*a*c,
-
-p1=(-b-sqrt(d))/2*a,
-p2=(-b+sqrt(d))/2*a,
+if d>0
+    p1=(-b-sqrt(d))/2*a;
+    p2=(-b+sqrt(d))/2*a;
+elseif d==0
+    p0=-b/(2*a);
+else
+    msg='brak miejsc zerowych dla podanych parametrow'
+    error(msg);
+end
 
 %check
 check=a*p1^2+b*p1+c,
 
-%1.2.1
-%test still sry
 whos
