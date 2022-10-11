0cb1b5b643d4242759c08f33666970bb3ddcb06b
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-10-11 18:30:07 +0200

    LAB_1 finished

--- LAB_1/task_1_2.m
+++ LAB_1/task_1_2.m
@@ -6,8 +6,10 @@ echo on
 %surface total
 s=200,
 syms r,
-v=1/2*(s*r-2*pi*r^3),
-dy=diff(v,'r'),
+r=1:0.05:5, %kroki co 0.05 od 1 do 5
+v=1/2*(s*r-2*pi*r.^3),
+plot(r,v),  %rysowanie wykresu
+%dy=diff(v,'r'),
 
 R = sqrt(s/(6*pi)),
 H =2*R,
