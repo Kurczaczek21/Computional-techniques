2f5666dfc63a7b6eaefb25ca6280f7b7a85a625f
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-10-18 15:04:55 +0200

    task 13 halfly done

--- LAB_2/task_13.m
+++ LAB_2/task_13.m
@@ -1,17 +1,49 @@
-% Rozwiązanie równania:
-% a*x^2 + b*x + c = 0
+close all;
+clear;
+clc;
 
-format long;
+% 1a. przypadek gdy b^2 >> 4ac czyli a = 1, b = 100, c = 1
+a1 = 1; b1 = 100; c1 = 1;
+[x1,x2] = ulepszona(a1,b1,c1)
+% 1b. ten sam przypadek ale normalnie liczone
+[x1,x2] = normalna(a1,b1,c1)
+% spoiler nie zgadza sie
 
-a=1.2, c = 3.4, b = -1e+6*(4*a*c), pause
+% 2a. przypadek gdy miejsca zerowe sa  bardzo blisko siebie (podejrzane)
+a2 = 1; b2 = 0; c2=0.0001;
+[x1,x2] = ulepszona(a2,b2,c2)
+% 2b. ten sam przypadek ale normalnie liczone
+[x1,x2] = normalna(a2,b2,c2)
+% spoiler wychodzi na to samo
 
-x1 = (-b+sqrt(b^2-4*a*c))/(2*a),
-x2 = (-b-sqrt(b^2-4*a*c))/(2*a), pause
+% 3a. przypadek klasyczny normalny wynik
+a3 = 2; b3 = 4; c3 = 2;
+[x1,x2] = ulepszona(a3,b3,c3)
+% 2b. ten sam przypadek ale normalnie liczone
+[x1,x2] = normalna(a3,b3,c3)
+% spoiler wychodzi na to samo bo liczby sa calkowite
 
-if( abs(x1) > abs(x2) )
-    x2new = 2*c /(-b+sqrt(b^2-4*a*c)),
-end    
-if( abs(x1) < abs(x2) )
-    x1new = 2*c /(-b-sqrt(b^2-4*a*c)),
-end   
-%kwadratowe.m ^^^
+
+
+
+
+function [x1 , x2] = ulepszona(a,b,c)
+    delta = (b*b) - (4*a*c);
+    x1 = ((-b) - sqrt(delta)) / (2*a);
+    x2 = ((-b) - sqrt(delta)) / (2*a);
+
+    if abs(x1) > abs(x2)
+        x2 = (2*c) / (-b - sqrt(delta));
+    else
+        x1 = (2*c) / (-b - sqrt(delta));
+    end
+end
+
+function [x1 , x2] = normalna(a,b,c)
+    delta = (b*b) - (4*a*c);
+    x1 = ((-b) - sqrt(delta)) / (2*a);
+    x2 = ((-b) - sqrt(delta)) / (2*a);
+end
+
+
+
