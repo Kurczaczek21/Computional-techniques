5a64b80e797ed86a85ad31dcef66dc2a8cba7e7a
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-10-25 16:40:05 +0200

    task 16 done

--- LAB_2/task_16.m
+++ LAB_2/task_16.m
@@ -2,12 +2,14 @@ clear all; close all;
 % 2.16
 a= 0.5;
 b= 1; 
-c = 0.490;
-cond = b/(sqrt(b^2-(4*a*c))),
-
+c = 0.5;
+cond = b/(sqrt(b^2-(4*a*c))), % DZIALA, dazy do inf zgodnie z oczekiwaniami
+                              % poniewaz dzielimy przez liczbe bliska 0
+                              % mały błąd => duża zmiana wyniku
+                   
 a = 0.5; 
 b = 1 + 0.001*randn(1,1000);
-c = 0.490;
+c = 0.001;
 suma=[];
 sumacond=[];
 for i = 1:size(b, 2)
@@ -23,9 +25,10 @@ disp("odchylenie rozwiazan: " + std(suma)),
 % Wyswietlanie sredniej i odchylenia standardowego uwarunkowan
 disp("srednia uwarunkowan: " + mean(sumacond)),
 disp("odchylenie uwarunkowan: " + std(sumacond)),
+disp(" "),
 
 %INNE c
-c=0.001;
+c=0.49;
 suma1=[];
 sumacond1=[];
 for i = 1:size(b, 2)
@@ -41,6 +44,7 @@ disp("odchylenie rozwiazan: " + std(suma1)),
 % uwarunkowanie
 disp("srednia uwarunkowan: " + mean(sumacond1)),
 disp("odchylenie uwarunkowan: " + std(sumacond1)),
+disp(" "),
 
 %INNE c
 c=0.500;
