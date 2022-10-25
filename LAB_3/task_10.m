fe2e6408e7289ee088599f3ae919b8532656a240
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-10-25 19:28:58 +0200

    lab 3 sth done

--- LAB_3/task_10.m
+++ LAB_3/task_10.m
@@ -1,10 +1,34 @@
 clear; close all;
 
+C = readtable ('babia_gora.dat');
+x1 =C.Var1;
+x=rot90(x1);
+
+y1=C.Var2;
+y=rot90(y1);
+
+z1=C.Var3;
+z=rot90(z1);
+
+zw=z(1,2),
+
+xlin = linspace(min(x), max(x), 100);
+ylin = linspace(min(y), max(y), 100);
+[X,Y] = meshgrid(xlin, ylin);
+% Z = griddata(x,y,z,X,Y,'natural');
+% Z = griddata(x,y,z,X,Y,'cubic');
+Z = griddata(x,y,z,X,Y,'v4');
+mesh(X,Y,Z)
+axis tight; hold on
+plot3(x,y,z,'.','MarkerSize',15)
+
+%rotation
+
 referencePoint1 = [1;
                    1;
                    1];
 
-plotOrigin([-5,5],[-5,5],[-5,5], [0,0,0]);
+plotOrigin([-500,500],[-500,500],[-500,500], [0,0,0]);
 axis off;
 plot3(referencePoint1(1),referencePoint1(2),referencePoint1(3), 'ok');
 
@@ -12,3 +36,5 @@ angle = 90 % in degrees
 
 newPoint1 = rotateMatrix('z', angle) * referencePoint1;
 plot3(newPoint1(1),newPoint1(2),newPoint1(3), 'ok');
+
+
