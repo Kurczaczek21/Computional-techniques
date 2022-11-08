8dda504f27e112301c8cc7f8e43f5868cd89e8c4
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-11-08 19:07:32 +0100

    lab no 3 done.

--- LAB_3/Lab3_8.m
+++ LAB_3/Lab3_8.m
@@ -1,34 +1,31 @@
 close all; clear all;
 
-% Dane wejsciowe słonia
-[e,fpr]=audioread('elephant.wav',[1,2^14]);
+[slon,fpr]=audioread('elephant.wav',[1,2^14]);
+[kanar,fpr]=audioread('canary.wav',[1,2^14]);
 
-% Dane wejsciowe kanarka
-[c,fpr]=audioread('canary.wav',[1,2^14]);
-
-% Zsumowany dzwiek zwierząt
-x = e + c;
+x = slon+kanar;
 N = length(x);
 
-% Wykres wygnału wejsciowego
+%syg wejsciowy
 figure; plot(x); title('x(n)'); %n czas
 
 
+%A to jest macierz ortogonalna 
+% Mnożenie ścieżki dźwiękowej, przez A daje nam widmo częstotliwościowe.
+% 
 % Transformacja liniowa/ortogonalna - ANALIZA
-n = 0:N-1;
-k = 0:N-1;
+n = 0:N-1; k = 0:N-1;
 A = sqrt(2/N) * cos(pi/N * (k'*n));
-% x = A(500,:) + A(1000,:); x = x'; -> to jest 1 podpunkt
+% x = A(500,:) + A(1000,:); x = x'; -> 1 podpunkt
 y = A*x;
 % wykres po transformacji ortogonalnej
 figure; plot(y); title('y(k)'); %k czestotliwosc
 
 
-% Modyfikacja wyniku
-% Tu mozna wybrac zakres dolny i gorny
-% Sam słoń to bedzie 1 do 3900 (znieksztalcony lekko)
-% Sam kanarek to bedzie od 4000 do 9500
-start = 3900; stop = 9500;
+% Modyfikacja wyniku -> 2
+% Sam słoń 1 do 3900 
+% Sam kanarek 4000 do 9500
+start = 4000; stop = 9500;
 
 y(1:start-1,1) = zeros(start-1,1);
 y(stop+1:N,1) = zeros(N-stop,1);
@@ -36,9 +33,9 @@ y(stop+1:N,1) = zeros(N-stop,1);
 figure; plot(y); title('y(k)');
 
 
-% Transformacja odwrotna - SYNTEZA
+% Transformacja odwrotna - SYNTEZA -> 3
 xback = A'*y;
 % Wykres po syntezie
 figure; plot(xback); title('xback(n)');
-soundsc(x,fpr); pause
+soundsc(x,fpr);
 soundsc(xback,fpr);
