8d15f7d95eb22e274630a8082a1e492a3156e483
Mateusz Warzecha PC <warzecha.mati@gmail.com>
2022-11-11 18:08:35 +0100

    working

--- LAB_4/task_5.m
+++ LAB_4/task_5.m
@@ -4,10 +4,10 @@ clear all; close all;
 
 A = [ 1 2;3 4 ];
 B = [ 5; 11 ];
-inv(A),
-x1 = inv(A)*B, % x=A^(-1)*b
+
+x1 = inv(A)*B; % x=A^(-1)*b
 A_1 = (1/( (A(1,1)*A(2,2) ) - ((A(2,1))*(A(1,2) ) ) ) ) *[A(2,2),-A(1,2);
-                        -A(2,1),A(1,1)],
+                        -A(2,1),A(1,1)];
 x1_1 = A_1*B;
 
 
@@ -17,16 +17,17 @@ x1_1 = A_1*B;
 % x3 = pinv(A)*b, % x = inv( A’*A ) * A’ * b , sprawdzisz?
 %bhat = A*x1, % sprawdzenie
 %err = max(abs(x1-x2)), % blad
-
+inv(A),
 inverse_matrix(A),
 
-function A_1 = inverse_matrix(A)
-    determinant = det(A);
-    matrix_size = size(A,1);
-    i=0;
-    for i < matrix_size 
-        x=12,
+function [A_1] = inverse_matrix(A)
+    AD = zeros(size(A));
+    for i = 1 : size(AD)
+        for j = 1: size(AD)
+            minor = A;
+            minor(i,:)=[]; minor(:,j)=[];
+            AD(i,j) = ((-1)^(i+j))*det(minor);
+        end
     end
-
-
+    A_1 = (1/det(A))*(AD.')
 end
