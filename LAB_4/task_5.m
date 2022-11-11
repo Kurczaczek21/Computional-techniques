ff551f89264793d09625084bb5c52364f3d83e9c
Mateusz Warzecha PC <warzecha.mati@gmail.com>
2022-11-11 18:32:33 +0100

    ok

--- LAB_4/task_5.m
+++ LAB_4/task_5.m
@@ -1,24 +1,18 @@
-% equnonlin_invmat.m
-% A*x = b, x=?
 clear all; close all;
 
 A = [ 1 2;3 4 ];
 B = [ 5; 11 ];
 
-x1 = inv(A)*B; % x=A^(-1)*b
+x1 = inv(A)*B; %#ok<MINV> % x=A^(-1)*b
+x2 = A\B; % optymalne rozwiazywanie rown. Ax=b
+% podpunkt 1
 A_1 = (1/( (A(1,1)*A(2,2) ) - ((A(2,1))*(A(1,2) ) ) ) ) *[A(2,2),-A(1,2);
                         -A(2,1),A(1,1)];
-x1_1 = A_1*B;
+x3 = A_1*B;
 
 
-
-%x2 = A\B, % optymalne rozwiazywanie rown. Ax=b
-
-% x3 = pinv(A)*b, % x = inv( A’*A ) * A’ * b , sprawdzisz?
-%bhat = A*x1, % sprawdzenie
-%err = max(abs(x1-x2)), % blad
-inv(A),
-inverse_matrix(A),
+% podpunkt 2
+inverse_matrix(A);
 
 function [A_1] = inverse_matrix(A)
     AD = zeros(size(A));
@@ -29,5 +23,5 @@ function [A_1] = inverse_matrix(A)
             AD(i,j) = ((-1)^(i+j))*det(minor);
         end
     end
-    A_1 = (1/det(A))*(AD.')
+    A_1 = (1/det(A))*(AD.');
 end
